07e0deeaec15d3de4229bbdc109339f9f71b046f
Beatriz-Gomes-Felix <78601515+Beatriz-Gomes-Felix@users.noreply.github.com>
2025-05-30 13:53:50 -0300

    Create Evento.cs

---
+++ Evento.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CadastroEventosMaui
+{
+    public class Evento
+    {
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataFim { get; set; }
+        public int NumeroParticipantes { get; set; }
+        public string Local { get; set; }
+        public double CustoPorParticipante { get; set; }
+
+        public int DuracaoDias => (DataFim - DataInicio).Days + 1;
+        public double CustoTotal => NumeroParticipantes * CustoPorParticipante;
+    }
+}
